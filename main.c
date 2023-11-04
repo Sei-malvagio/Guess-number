a0838bcfed4bb9e776538fd15008afa7fc7772cd
Sei malvagio <145441542+Sei-malvagio@users.noreply.github.com>
2023-11-04 17:38:04 +0700

    Update main.c

--- main.c
+++ main.c
@@ -11,9 +11,10 @@ int main() {
 }
 
 int guessNumber(int guessNum) {
-    for(int i = 0; i < 3; i++) printf("\033[0;34m\033[1m=");
-    printf("\n\n\033[0;34m\033[1m Guess the number between 1-100 \033[0;97m");
-    for(int i = 0; i < 3; i++) printf("\033[0;34m\033[1m=\033[0;97m");
+    puts("\n\n")
+    for(int i = 0; i < 3; i++) printf("\033[0;33m\033[1m=");
+    printf("\033[0;33m\033[1m Guess the number between 1-100 \033[0;97m");
+    for(int i = 0; i < 3; i++) printf("\033[0;33m\033[1m=\033[0;97m");
     while(1) {
         srand(time(NULL));
         int randomNum = rand() % 100 + 1;
